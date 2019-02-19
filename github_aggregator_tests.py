6c21bde225be0412cff8faed8d4a36aceeba856d
Kyle <kyle.rozanitis@gmail.com>
2019-02-19 16:49:59 -0500

    added tests for function to gather commits

--- github_aggregator_tests.py
+++ github_aggregator_tests.py
@@ -7,7 +7,7 @@ import json
 import unittest
 
 # File Imports
-from github_aggregator import gather_repos
+from github_aggregator import gather_repos, gather_commits
 
 class TestGitHubAggregator(unittest.TestCase):
     """ This class contains all of the unit tests for the GitHub Aggregator """
@@ -16,6 +16,11 @@ class TestGitHubAggregator(unittest.TestCase):
         self.assertEqual(gather_repos('kylerozanitis'), ['DSA', 'expressionevaluator', 'gedcom-parser', 'GitHubApi567', 'hello-world', 'Personal-Website', 'school-data-management-system', 'SSW-567', 'Triangle567'])
         self.assertNotEqual(gather_repos('kylerozanitis'), ['expressionevaluator', 'gedcom-parser', 'GitHubApi567', 'hello-world', 'Personal-Website', 'school-data-management-system', 'SSW-567', 'Triangle567'])
         self.assertEqual(gather_repos('Asupkay'), ['agar.io-clone', 'asupkay.github.io', 'Chirp', 'Code-Jam-Parody', 'Cryptocurrency-Tracker-Vector', 'CryptoPortfolio', 'CS-546-Final-Project', 'CS-546-Web-Programming-I', 'CS-554-Web-Programming-II', 'DiceRoll', 'enlistment', 'friendly-js-primer', 'GameJam7', 'GCP-App-Engine-Node-Tutorial', 'Github-Monitor', 'GitHubApi567', 'Google-Chrome-Extensions', 'Homeless-Shelter-Finder', 'Interview-Practice', 'Interview-Prep', 'NodeJSPractice', 'Raffler', 'React-Redux-Demo', 'Smart-Home', 'SSW-567', 'SSW-567-HW-02a', 'Stock-Tracker', 'ToDone-App', 'ud851-Exercises', 'ud851-Sunshine'])
+
+    def test_gather_commits(self):
+        self.assertEqual(gather_commits('kylerozanitis', 'Triangle567'), ('kylerozanitis', 11))
+        self.assertNotEqual(gather_commits('kylerozanitis', 'Triangle567'), ('kylerozanitis', 13))
+        self.assertEqual(gather_commits('Asupkay', 'Chirp'), ('Asupkay', 2))
 
 if __name__ == '__main__':
     print("Running Unit Tests...")
