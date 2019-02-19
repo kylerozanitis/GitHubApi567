9bf1c43d7776b83cbfed3ef7382107e0147979a6
Kyle <kyle.rozanitis@gmail.com>
2019-02-19 16:02:49 -0500

    adding function to collect repos

---
+++ github_aggregator.py
@@ -0,0 +1,27 @@
+""" This program requests repository and commit information from GitHub using
+the requests & json modules and the public GitHub API. """
+
+# Modules
+import requests
+import json
+
+
+def gather_repos(github_id):
+    """ This function takes a username as input and returns a list of
+    repositories found in Github for that user ID. """
+    repos_list = []
+
+    url = 'https://api.github.com/users/' + 'kylerozanitis' + '/repos'
+    response = requests.get(url)
+    
+    repos = json.loads(response.text)
+
+    for repo in repos:
+        if repo['name']:
+            repos_list.append(repo['name'])
+        elif repo['full_name']:
+            repos_list.append(repo['full_name'])
+        else:
+            repos_list.append(repo['id'])
+
+    print(repos_list)
